c88ea3caf1aee085f94899f23ecd7f9830090dfe
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2025-02-06 11:42:15 +0000

    Week 3 S00237686 Lab 1 Finished

--- RAD302Week3Lab12025WebAPIS00237686/Controllers/CustomerController.cs
+++ RAD302Week3Lab12025WebAPIS00237686/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RAD302Week3Lab12025CL.S00237686;
 using Tracker.WebAPIClient;
 
@@ -21,6 +22,40 @@ namespace RAD302Week3Lab12025WebAPIS00237686.Controllers
             ActivityAPIClient.Track(StudentID: "S00237686", StudentName: "Tristan Cawley",
                 activityName: "Rad302 Week 3 Lab 1", Task: "Testing Basic Controller Call");
             return _repository.GetAll();
+        }
+
+        // Get Customer By ID
+        [HttpGet("GetCustomerbyID/{id:int}")]
+        public ActionResult<Customer> GetCustomerbyID(int id)
+        {
+            var customer = _repository.Get(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            ActivityAPIClient.Track(StudentID: "S00237686", StudentName: "Tristan Cawley",
+                activityName: "Rad302 Week 3 Lab 1", Task: "Testing Get Customer By ID Call");
+
+            return Ok(customer);
+        }
+
+        // Check Customer Credit
+        [HttpGet("checkcredit/{id}/{amount}")]
+        public ActionResult<bool> CheckCustomerCredit(int id, decimal amount)
+        {
+            var customer = _repository.Get(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            bool result = amount < (decimal)customer.CreditRating;
+
+            ActivityAPIClient.Track(StudentID: "S00237686", StudentName: "Tristan Cawley",
+                activityName: "Rad302 Week 3 Lab 1", Task: "Testing Check Customer Credit Rating Call");
+
+            return Ok(result);
         }
     }
 }
---
+++ RAD302Week3Lab12025WebAPIS00237686/Controllers/GetCustomerByID.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tracker.WebAPIClient;
+
+namespace RAD302Week3Lab12025WebAPIS00237686.Controllers
+{
+    public class GetCustomerByID : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ActivityAPIClient.Track(StudentID: "S00237686", StudentName: "Tristan Cawley",
+    activityName: "Rad302 Week 3 Lab 1", Task: "Testing Check Customer Credit Rating Call");
+            return View();
+        }
+    }
+}
