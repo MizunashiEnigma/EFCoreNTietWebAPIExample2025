a263ba619eb30ec944d5de87a49747d1a1d48f0d
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2025-03-13 10:50:14 +0000

    Week 8 Data Repository implemented S00237686

---
+++ ProductWebAPI2025/Controllers/GRNController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tracker.WebAPIClient;
+using Week8ProductModelS00237686;
+
+namespace Week8ProductWebAPI2025S00237686.Controllers
+{
+    public class GRNController : Controller
+    {
+        private readonly IGRN<GRN> _repository;
+        public GRNController(IGRN<GRN> repository)
+        {
+            _repository = repository;
+
+            ActivityAPIClient.Track(StudentID: "S00237686", StudentName: "Tristan Cawley", activityName: "RAD30223 Week 8 Lab 1", Task: "Testing GRN Controller Actions");
+        }
+
+        public IEnumerable<GRN> Get()
+        {
+            return _repository.GetAll();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
